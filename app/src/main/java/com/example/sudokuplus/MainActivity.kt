961a3fa90a62f0c3ea742dd0a8a2bb1e826b82bc
Arun Sharma <arun243sharma9@gmail.com>
2024-02-13 17:31:46 +0530

    Added GameScreen

--- app/src/main/java/com/example/sudokuplus/MainActivity.kt
+++ app/src/main/java/com/example/sudokuplus/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sudokuplus
 
+import GameScreen
+import Timer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,13 +10,14 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
-    lateinit var navController: NavHostController
+   // lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            installSplashScreen()
-            navController = rememberNavController()
-            SetupNavGraph(navController = navController)
+//            installSplashScreen()
+//            navController = rememberNavController()
+//            SetupNavGraph(navController = navController)
+            GameScreen()
         }
     }
 }
